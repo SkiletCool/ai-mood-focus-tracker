d1f905041893a42f20ccbe8d4bfe5f05708bd3dd
SkiletCool-desing <skiletcool110706@gmail.com>
2026-01-12 12:13:53 +0200

    add mood & focus logic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 console.log('App initialized');
+const focusRange = document.getElementById('focusRange');
+const focusValue = document.getElementById('focusValue');
+
+focusRange.addEventListener('input', () => {
+  focusValue.textContent = focusRange.value;
+});
